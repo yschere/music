0c9384da25111a4cc5ec0c0bd04c2e0cdd4801db
yschere <6558581+yschere@users.noreply.github.com>
2025-08-11 22:52:31 -0500

    changed logging for GetSongDataV2 to android util Log

--- core/domain/src/main/java/com/example/music/domain/usecases/GetSongDataV2.kt
+++ core/domain/src/main/java/com/example/music/domain/usecases/GetSongDataV2.kt
@@ -1,5 +1,6 @@
 package com.example.music.domain.usecases
 
+import android.util.Log
 import com.example.music.domain.model.SongInfo
 import com.example.music.domain.model.asExternalModel
 import com.example.music.domain.util.MediaRepo
@@ -21,22 +22,28 @@ class GetSongDataV2 @Inject constructor(
 ) {
     // use to build SongInfo from an Audio id
     operator fun invoke(songId: Long): Flow<SongInfo> {
-        domainLogger.info { "$TAG - 1 song - start\n" +
-                "songId: $songId" }
+        Log.i(TAG, "Fetching Data for single song - start\n" +
+                "songId: $songId")
 
         return resolver.getAudioFlow(songId)
             .map {
+                Log.i(TAG, "Found file data for song $songId\n" +
+                        "Title: ${it.title}\n" +
+                        "Artist: ${it.artist}\n" +
+                        "Album: ${it.album}")
                 it.asExternalModel()
             }
     }
 
     // use to build list of SongInfo from list of Audio ids
     operator fun invoke(songIds: List<Long>): Flow<List<SongInfo>> {
-        domainLogger.info { "$TAG - multi songs - start\n" +
-                "songs size: ${songIds.size}" }
+        Log.i(TAG, "Fetching data for multiple songs - start\n" +
+                "songs size: ${songIds.size}")
         return resolver.getAudiosFlow(songIds)
             .map { songList ->
+                Log.i(TAG, "Found file data for multiple songs --- ")
                 songList.map { song ->
+                    Log.i(TAG, "Song: ${song.id} - ${song.title}")
                     song.asExternalModel()
                 }
             }
