2b363d7846a7b09eeb5879095c96a37386034ba5
yschere <6558581+yschere@users.noreply.github.com>
2025-08-12 12:17:51 -0500

    Deleted unused/obsolete domain use cases; changed logging to android util Log

--- core/domain/src/main/java/com/example/music/domain/model/ArtistGenreFilterResult.kt
+++
@@ -1,9 +0,0 @@
-package com.example.music.domain.model
-
-/**
- * Leftover domain model reinterpretation from Jetcaster to Musicality
- */
-data class ArtistGenreFilterResult(
-    val topArtists: List<ArtistInfo> = emptyList(),
-    val songs: List<SongInfo> = emptyList()
-)
---
+++ core/domain/src/main/java/com/example/music/domain/model/SearchQueryFilterV2.kt
@@ -0,0 +1,10 @@
+package com.example.music.domain.model
+
+/**
+ * Domain model to define search query results structure.
+ */
+data class SearchQueryFilterV2 (
+    val songs: List<SongInfo> = emptyList(),
+    val artists: List<ArtistInfo> = emptyList(),
+    val albums: List<AlbumInfo> = emptyList(),
+)
