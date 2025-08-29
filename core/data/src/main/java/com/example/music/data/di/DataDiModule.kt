6d63f6df460933472b0788476515d0b6fb3a8148
yschere <6558581+yschere@users.noreply.github.com>
2025-08-29 18:17:08 -0500

    Added loggers to dependency injector modules for UI, Domain, and Room & DataStore in Data

--- core/data/src/main/java/com/example/music/data/di/DataDiModule.kt
+++ core/data/src/main/java/com/example/music/data/di/DataDiModule.kt
@@ -1,6 +1,7 @@
 package com.example.music.data.di
 
 import android.content.Context
+import android.util.Log
 import androidx.datastore.core.DataStore
 import androidx.datastore.preferences.core.PreferenceDataStoreFactory
 import androidx.datastore.preferences.core.Preferences
@@ -42,6 +43,7 @@ import kotlinx.coroutines.SupervisorJob
 import javax.inject.Singleton
 
 private const val APP_PREFERENCES = "app_preferences.pb"
+private const val TAG = "Data-DI-Module"
 
 @Module
 @InstallIn(SingletonComponent::class)
@@ -57,31 +59,29 @@ object DataDiModule {
     @Singleton
     fun provideMainDispatcher(): CoroutineDispatcher = Dispatchers.Main
 
-//    @Provides
-//    @Singleton
-//    fun provideOkHttpClient(
-//        @ApplicationContext context: Context
-//    ): OkHttpClient = OkHttpClient.Builder()
-//        .cache(Cache(File(context.cacheDir, "http_cache"), (20 * 1024 * 1024).toLong()))
-//        .apply {
-//            if (BuildConfig.DEBUG) eventListenerFactory(LoggingEventListener.Factory())
-//        }
-//        .build() // Question: do i still need this if I do not want my app to rely on network calls?
+    /*@Provides
+    @Singleton
+    fun provideOkHttpClient(
+        @ApplicationContext context: Context
+    ): OkHttpClient = OkHttpClient.Builder()
+        .cache(Cache(File(context.cacheDir, "http_cache"), (20 * 1024 * 1024).toLong()))
+        .apply {
+            if (BuildConfig.DEBUG) eventListenerFactory(LoggingEventListener.Factory())
+        }
+        .build() // Question: do i still need this if I do not want my app to rely on network calls?*/
 
     @Provides
     @Singleton
     fun provideDatabase(
         @ApplicationContext context: Context
-    ): MusicDatabase =
-        Room.databaseBuilder(context, MusicDatabase::class.java, "music.db")
-            // This is not recommended for normal apps, but the goal of this sample isn't to
-            // showcase all of Room.
-            // Question: why is this not recommended for normal apps?
-
+    ): MusicDatabase {
+        Log.i(TAG, "Providing Music Database")
+        return Room.databaseBuilder(context, MusicDatabase::class.java, "music.db")
             .fallbackToDestructiveMigration()
             .createFromAsset("preview_data.db")
             //.fallbackToDestructiveMigration()
             .build()
+    }
 
     /* //coil image loader
     @Provides
@@ -97,21 +97,24 @@ object DataDiModule {
     @Singleton
     fun providePreferencesDataStore(
         @ApplicationContext appContext: Context
-    ): DataStore<Preferences> = PreferenceDataStoreFactory.create(
-        /*corruptionHandler = ReplaceFileCorruptionHandler(
-            produceNewData = { emptyPreferences() }
-        ),
-        migrations = listOf(
-            object : DataMigration<Preferences> {
-                override suspend fun cleanUp() { TODO("clean up Not yet implemented") }
-                override suspend fun migrate(currentData: Preferences): Preferences { TODO("migrate Not yet implemented") }
-                override suspend fun shouldMigrate(currentData: Preferences): Boolean { TODO("should migrate Not yet implemented") }
-            },
-        ),*/
-        corruptionHandler = null,
-        scope = CoroutineScope(Dispatchers.IO + SupervisorJob()),
-        produceFile = { appContext.preferencesDataStoreFile(APP_PREFERENCES) }
-    )
+    ): DataStore<Preferences> {
+        Log.i(TAG, "Providing Preferences DataStore")
+        return PreferenceDataStoreFactory.create(
+            /*corruptionHandler = ReplaceFileCorruptionHandler(
+                produceNewData = { emptyPreferences() }
+            ),
+            migrations = listOf(
+                object : DataMigration<Preferences> {
+                    override suspend fun cleanUp() { TODO("clean up Not yet implemented") }
+                    override suspend fun migrate(currentData: Preferences): Preferences { TODO("migrate Not yet implemented") }
+                    override suspend fun shouldMigrate(currentData: Preferences): Boolean { TODO("should migrate Not yet implemented") }
+                },
+            ),*/
+            corruptionHandler = null,
+            scope = CoroutineScope(Dispatchers.IO + SupervisorJob()),
+            produceFile = { appContext.preferencesDataStoreFile(APP_PREFERENCES) }
+        )
+    }
 
     @Provides
     @Singleton
--- core/domain/src/main/java/com/example/music/domain/di/DomainDiModule.kt
+++ core/domain/src/main/java/com/example/music/domain/di/DomainDiModule.kt
@@ -4,6 +4,8 @@ import dagger.Module
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
 
+private const val TAG = "Domain-DI-Module"
+
 @Module
 @InstallIn(SingletonComponent::class)
 object DomainDiModule {
