a4ebb238c01871b22aeb2fefb98f2c68b37e5191
yschere <6558581+yschere@users.noreply.github.com>
2025-09-14 17:24:17 -0500

    Revised LibraryScreen with updated Song, Album, Artist modal selection and action logic

--- app/src/main/java/com/example/music/ui/library/LibraryViewModel.kt
+++ app/src/main/java/com/example/music/ui/library/LibraryViewModel.kt
@@ -4,7 +4,6 @@ import android.util.Log
 import androidx.compose.runtime.Immutable
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import com.example.music.data.database.model.Song
 import com.example.music.domain.usecases.GetLibraryComposersUseCase
 import com.example.music.domain.usecases.GetLibraryPlaylistsUseCase
 import com.example.music.domain.usecases.GetAppPreferencesUseCase
@@ -14,8 +13,9 @@ import com.example.music.domain.model.ComposerInfo
 import com.example.music.domain.model.GenreInfo
 import com.example.music.domain.model.PlaylistInfo
 import com.example.music.domain.model.SongInfo
-//import com.example.music.domain.player.SongPlayer
 import com.example.music.data.util.combine
+import com.example.music.domain.usecases.GetAlbumDetailsV2
+import com.example.music.domain.usecases.GetArtistDetailsV2
 import com.example.music.domain.usecases.GetLibraryAlbumsV2
 import com.example.music.domain.usecases.GetLibraryArtistsV2
 import com.example.music.domain.usecases.GetLibraryGenresV2
@@ -27,10 +27,9 @@ import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.catch
+import kotlinx.coroutines.flow.first
 import kotlinx.coroutines.launch
 import javax.inject.Inject
-
-private const val TAG = "Library View Model"
 
 /** Changelog:
  *
@@ -43,6 +42,25 @@ private const val TAG = "Library View Model"
  * 7/22-23/2025 - Removed PlayerSong completely
  */
 
+private const val TAG = "Library View Model"
+
+data class LibraryScreenUiState(
+    val isLoading: Boolean = true,
+    val errorMessage: String? = null,
+    val libraryCategories: List<LibraryCategory> = emptyList(),
+    val selectedLibraryCategory: LibraryCategory = LibraryCategory.Playlists,
+    val libraryAlbums: List<AlbumInfo> = emptyList(),
+    val libraryArtists: List<ArtistInfo> = emptyList(),
+    val libraryComposers: List<ComposerInfo> = emptyList(),
+    val libraryGenres: List<GenreInfo> = emptyList(),
+    val libraryPlaylists: List<PlaylistInfo> = emptyList(),
+    val librarySongs: List<SongInfo> = emptyList(),
+    val totals: List<Int> = emptyList(),
+    val selectSong: SongInfo = SongInfo(),
+    val selectAlbum: AlbumInfo = AlbumInfo(),
+    val selectArtist: ArtistInfo = ArtistInfo()
+)
+
 @HiltViewModel
 class LibraryViewModel @Inject constructor(
     getLibrarySongsV2: GetLibrarySongsV2,
@@ -53,6 +71,9 @@ class LibraryViewModel @Inject constructor(
     getLibraryAlbumsV2: GetLibraryAlbumsV2,
     getTotalCountsV2: GetTotalCountsV2,
     getAppPreferences: GetAppPreferencesUseCase,
+
+    private val getAlbumDetailsV2: GetAlbumDetailsV2,
+    private val getArtistDetailsV2: GetArtistDetailsV2,
     private val songController: SongController
 ) : ViewModel() {
     /* ------ Current running UI needs:  ------
@@ -93,7 +114,6 @@ class LibraryViewModel @Inject constructor(
     private val sortedGenres = getLibraryGenresUseCase("name", true)
         .shareIn(viewModelScope, SharingStarted.WhileSubscribed())*/
 
-
     /* ------ Objects used in previous iterations:  ------
     private val songs = songRepo.getAllSongs()
         .shareIn(viewModelScope, SharingStarted.WhileSubscribed())
@@ -110,7 +130,11 @@ class LibraryViewModel @Inject constructor(
 
     // Holds the sorting preferences saved in the data store
     //private val sortPrefs = getAppPreferences()
-    private val showBottomSheet = MutableStateFlow(false)
+
+    // Holds the selected items values to show in more options modals
+    private val selectedSong = MutableStateFlow<SongInfo?>(null)
+    private val selectedAlbum = MutableStateFlow<AlbumInfo?>(null)
+    private val selectedArtist = MutableStateFlow<ArtistInfo?>(null)
 
     // Holds our view state which the UI collects via [state]
     private val _state = MutableStateFlow(LibraryScreenUiState())
@@ -122,18 +146,15 @@ class LibraryViewModel @Inject constructor(
         get() = _state
 
     init {
+        Log.i(TAG, "init START")
         viewModelScope.launch {
-            Log.i(TAG, "viewModelScope launch start")
+            Log.i(TAG, "viewModelScope launch START")
             val counts = getTotalCountsV2()
 
-            // Combines the latest value from each of the flows, allowing us to generate a
-            // view state instance which only contains the latest values.
             combine(
+                refreshing,
                 libraryCategories,
                 selectedLibraryCategory,
-                refreshing,
-                showBottomSheet,
-//                appPreferences.appPreferencesFlow,
                 appPreferencesFlow,
                 //getLibraryAlbumsUseCase("title", true),//sortedAlbums,
                 //getLibraryAlbumsV2("title", true),
@@ -150,6 +171,9 @@ class LibraryViewModel @Inject constructor(
 
                 //getLibrarySongsUseCase("title", true),//sortedSongs,
                 //getLibrarySongsV2("title", true),
+                selectedSong,
+                selectedAlbum,
+                selectedArtist,
             ) {
 
             /*combine(
@@ -175,10 +199,9 @@ class LibraryViewModel @Inject constructor(
                     getLibrarySongsUseCase(values.songSortOrder.name, values.isSongAsc)//sortedSongs,
                 },
             ){*/
+                refreshing,
                 libraryCategories,
                 libraryCategory,
-                refreshing,
-                showBottomSheet,
                 appPreferences,
                 //libraryAlbums,
                 //libraryArtists,
@@ -186,12 +209,15 @@ class LibraryViewModel @Inject constructor(
                 //libraryGenres,
                 libraryPlaylists,
                 //librarySongs,
+                selectSong,
+                selectAlbum,
+                selectArtist
                 ->
 
-                Log.i(TAG, "LibraryScreenUiState:")
-                Log.i(TAG, "isLoading: $refreshing")
-                Log.i(TAG, "libraryCategories: $libraryCategories")
-                Log.i(TAG, "selectedLibraryCategory: $libraryCategory")
+                Log.i(TAG, "LibraryScreenUiState combine START:\n" +
+                    "isLoading: $refreshing\n" +
+                    "libraryCategories: $libraryCategories\n" +
+                    "selectedLibraryCategory: $libraryCategory\n")
                 var libraryAlbums: List<AlbumInfo> = emptyList()
                 var libraryArtists: List<ArtistInfo> = emptyList()
                 //var libraryComposers: List<ComposerInfo> = emptyList()
@@ -239,7 +265,9 @@ class LibraryViewModel @Inject constructor(
                     libraryPlaylists = libraryPlaylists,
                     librarySongs = librarySongs,
                     totals = counts,
-                    showBottomModal = showBottomSheet,
+                    selectSong = selectSong ?: SongInfo(),
+                    selectAlbum = selectAlbum ?: AlbumInfo(),
+                    selectArtist = selectArtist ?: ArtistInfo()
                 )
             }.catch { throwable ->
                 Log.i(TAG, "Error Caught: ${throwable.message}")
@@ -253,8 +281,8 @@ class LibraryViewModel @Inject constructor(
                 _state.value = it
             }
         }
-
         refresh(force = false)
+        Log.i(TAG, "init END")
     }
 
     fun refresh(force: Boolean = true) {
@@ -264,7 +292,6 @@ class LibraryViewModel @Inject constructor(
             runCatching {
                 Log.i(TAG, "Refresh runCatching")
                 refreshing.value = true
-                //podcastsRepository.updatePodcasts(force)
             }.onFailure {
                 Log.i(TAG, "$it ::: runCatching failed (not sure what this means)")
             }
@@ -275,15 +302,30 @@ class LibraryViewModel @Inject constructor(
     }
 
     fun onLibraryAction(action: LibraryAction) {
+        Log.i(TAG, "onLibraryAction - $action")
         when (action) {
             is LibraryAction.LibraryCategorySelected -> onLibraryCategorySelected(action.libraryCategory)
-            //maybe filtering / sorting selections added here?
-            is LibraryAction.PlaySongs -> onPlaySongs(action.songs)
+            is LibraryAction.AlbumMoreOptionsClicked -> onAlbumMoreOptionsClicked(action.album)
+            is LibraryAction.ArtistMoreOptionsClicked -> onArtistMoreOptionsClicked(action.artist)
+            is LibraryAction.SongMoreOptionsClicked -> onSongMoreOptionsClicked(action.song)
+
+            is LibraryAction.PlaySong -> onPlaySong(action.song)
+            is LibraryAction.PlaySongNext -> onPlaySongNext(action.song)
             is LibraryAction.QueueSong -> onQueueSong(action.song)
+
+            is LibraryAction.PlaySongs -> onPlaySongs(action.songs)
             is LibraryAction.QueueSongs -> onQueueSongs(action.songs)
-            is LibraryAction.ShowModal -> onShowModal(action.libraryCategory, action.isModalOpen)
             is LibraryAction.ShuffleSongs -> onShuffleSongs(action.songs)
-            is LibraryAction.SongClicked -> onSongClicked(action.song)
+
+            is LibraryAction.PlayAlbum -> onPlayAlbum(action.album)
+            is LibraryAction.PlayAlbumNext -> onPlayAlbumNext(action.album)
+            is LibraryAction.ShuffleAlbum -> onShuffleAlbum(action.album)
+            is LibraryAction.QueueAlbum -> onQueueAlbum(action.album)
+
+            is LibraryAction.PlayArtist -> onPlayArtist(action.artist)
+            is LibraryAction.PlayArtistNext -> onPlayArtistNext(action.artist)
+            is LibraryAction.ShuffleArtist -> onShuffleArtist(action.artist)
+            is LibraryAction.QueueArtist -> onQueueArtist(action.artist)
         }
     }
 
@@ -292,36 +334,101 @@ class LibraryViewModel @Inject constructor(
         refresh()
     }
 
-    private fun onPlaySongs(songs: List<SongInfo>) {
-        Log.i(TAG, "onPlaySongs -> ${songs.size}")
-        songController.play(songs)
+    private fun onAlbumMoreOptionsClicked(album: AlbumInfo) {
+        Log.i(TAG, "onAlbumMoreOptionClick -> ${album.title}")
+        selectedAlbum.value = album
+    }
+    private fun onArtistMoreOptionsClicked(artist: ArtistInfo) {
+        Log.i(TAG, "onArtistMoreOptionClick -> ${artist.name}")
+        selectedArtist.value = artist
+    }
+    private fun onSongMoreOptionsClicked(song: SongInfo) {
+        Log.i(TAG, "onSongMoreOptionClick -> ${song.title}")
+        selectedSong.value = song
     }
 
+    private fun onPlaySong(song: SongInfo) {
+        Log.i(TAG, "onPlaySong - ${song.title}")
+        songController.play(song)
+    }
+    private fun onPlaySongNext(song: SongInfo) {
+        Log.i(TAG, "onPlaySongNext - ${song.title}")
+        songController.addToQueueNext(song)
+    }
     private fun onQueueSong(song: SongInfo) {
         Log.i(TAG, "onQueueSong -> ${song.title}")
         songController.addToQueue(song)
     }
 
+    private fun onPlaySongs(songs: List<SongInfo>) {
+        Log.i(TAG, "onPlaySongs -> ${songs.size}")
+        songController.play(songs)
+    }
+    private fun onShuffleSongs(songs: List<SongInfo>) {
+        Log.i(TAG, "onShuffleSongs -> ${songs.size}")
+        songController.shuffle(songs)
+    }
     private fun onQueueSongs(songs: List<SongInfo>) {
         Log.i(TAG, "onQueueSongs -> ${songs.size}")
         songController.addToQueue(songs)
     }
 
-    private fun onShowModal(libraryCategory: LibraryCategory, isModalOpen: Boolean) {
-        //what is the purpose of this function?
-        // want to know which screen on library is being shown
-        // want to have a way for contexted modal? how would this accomplish it tho
-        showBottomSheet.value = isModalOpen
+    private fun onPlayAlbum(album: AlbumInfo) {
+        Log.i(TAG, "onPlaySongs -> ${album.title}")
+        viewModelScope.launch {
+            val songs = getAlbumDetailsV2(album.id).first().songs
+            songController.play(songs)
+        }
+    }
+    private fun onPlayAlbumNext(album: AlbumInfo) {
+        Log.i(TAG, "onPlayAlbumNext -> ${album.title}")
+        viewModelScope.launch {
+            val songs = getAlbumDetailsV2(album.id).first().songs
+            songController.addToQueueNext(songs)
+        }
+    }
+    private fun onShuffleAlbum(album: AlbumInfo) {
+        Log.i(TAG, "onShuffleAlbum -> ${album.title}")
+        viewModelScope.launch {
+            val songs = getAlbumDetailsV2(album.id).first().songs
+            songController.shuffle(songs)
+        }
+    }
+    private fun onQueueAlbum(album: AlbumInfo) {
+        Log.i(TAG, "onQueueAlbum -> ${album.title}")
+        viewModelScope.launch {
+            val songs = getAlbumDetailsV2(album.id).first().songs
+            songController.addToQueue(songs)
+        }
     }
 
-    private fun onShuffleSongs(songs: List<SongInfo>) {
-        Log.i(TAG, "onShuffleSongs -> ${songs.size}")
-        songController.shuffle(songs)
+    private fun onPlayArtist(artist: ArtistInfo) {
+        Log.i(TAG, "onPlayArtist -> ${artist.name}")
+        viewModelScope.launch {
+            val songs = getArtistDetailsV2(artist.id).first().songs
+            songController.play(songs)
+        }
     }
-
-    private fun onSongClicked(song: SongInfo) {
-        Log.i(TAG, "onSongClicked -> ${song.title}")
-        songController.play(song)
+    private fun onPlayArtistNext(artist: ArtistInfo) {
+        Log.i(TAG, "onPlayArtistNext -> ${artist.name}")
+        viewModelScope.launch {
+            val songs = getArtistDetailsV2(artist.id).first().songs
+            songController.addToQueueNext(songs)
+        }
+    }
+    private fun onShuffleArtist(artist: ArtistInfo) {
+        Log.i(TAG, "onShuffleArtist -> ${artist.name}")
+        viewModelScope.launch {
+            val songs = getArtistDetailsV2(artist.id).first().songs
+            songController.shuffle(songs)
+        }
+    }
+    private fun onQueueArtist(artist: ArtistInfo) {
+        Log.i(TAG, "onQueueArtist -> ${artist.name}")
+        viewModelScope.launch {
+            val songs = getArtistDetailsV2(artist.id).first().songs
+            songController.addToQueue(songs)
+        }
     }
 }
 
@@ -332,28 +439,29 @@ enum class LibraryCategory {
 @Immutable
 sealed interface LibraryAction {
     data class LibraryCategorySelected(val libraryCategory: LibraryCategory) : LibraryAction
+    data class AlbumMoreOptionsClicked(val album: AlbumInfo) : LibraryAction
+    data class ArtistMoreOptionsClicked(val artist: ArtistInfo) : LibraryAction
+    data class SongMoreOptionsClicked(val song: SongInfo) : LibraryAction
+
+    data class PlaySong(val song: SongInfo) : LibraryAction // songMO-play
+    data class PlaySongNext(val song: SongInfo) : LibraryAction // songMO-playNext
+    data class QueueSong(val song: SongInfo) : LibraryAction // songMO-queue
+
     data class PlaySongs(val songs: List<SongInfo>) : LibraryAction
-    data class QueueSong(val song: SongInfo) : LibraryAction
     data class QueueSongs(val songs: List<SongInfo>) : LibraryAction
-    data class ShowModal(val libraryCategory: LibraryCategory, val isModalOpen: Boolean) : LibraryAction
     data class ShuffleSongs(val songs: List<SongInfo>) : LibraryAction
-    data class SongClicked(val song: SongInfo) : LibraryAction
+
+    data class PlayAlbum(val album: AlbumInfo) : LibraryAction
+    data class PlayAlbumNext(val album: AlbumInfo) : LibraryAction
+    data class ShuffleAlbum(val album: AlbumInfo) : LibraryAction
+    data class QueueAlbum(val album: AlbumInfo) : LibraryAction
+
+    data class PlayArtist(val artist: ArtistInfo) : LibraryAction
+    data class PlayArtistNext(val artist: ArtistInfo) : LibraryAction
+    data class ShuffleArtist(val artist: ArtistInfo) : LibraryAction
+    data class QueueArtist(val artist: ArtistInfo) : LibraryAction
 }
 
-data class LibraryScreenUiState(
-    val isLoading: Boolean = true,
-    val errorMessage: String? = null,
-    val libraryCategories: List<LibraryCategory> = emptyList(),
-    val selectedLibraryCategory: LibraryCategory = LibraryCategory.Playlists,
-    val libraryAlbums: List<AlbumInfo> = emptyList(),
-    val libraryArtists: List<ArtistInfo> = emptyList(),
-    val libraryComposers: List<ComposerInfo> = emptyList(),
-    val libraryGenres: List<GenreInfo> = emptyList(),
-    val libraryPlaylists: List<PlaylistInfo> = emptyList(),
-    val librarySongs: List<SongInfo> = emptyList(),
-    val totals: List<Int> = emptyList(),
-    val showBottomModal: Boolean = false,
-)
 /* sealed interface LibraryScreenUiState {
     data object Loading : LibraryScreenUiState
     data class Ready(
