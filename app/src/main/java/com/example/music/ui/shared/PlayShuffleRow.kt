ede9f3b1f6e8714b2460fee9e411a05aa1ff33ab
yschere <6558581+yschere@users.noreply.github.com>
2025-09-15 02:49:11 -0500

    Created new files for reusable Composables ItemCountAndSortSelect and PlayShuffle button rows

---
+++ app/src/main/java/com/example/music/ui/shared/PlayShuffleRow.kt
@@ -0,0 +1,62 @@
+package com.example.music.ui.shared
+
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.PlayArrow
+import androidx.compose.material.icons.filled.Shuffle
+import androidx.compose.material3.Button
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
+import com.example.music.R
+import com.example.music.designsys.theme.MusicShapes
+
+/**
+ * Content section: Play Items and Shuffle Items buttons
+ */
+@Composable
+fun PlayShuffleButtons(
+    onPlayClick: () -> Unit,
+    onShuffleClick: () -> Unit,
+    modifier: Modifier = Modifier,
+) {
+    Row(
+        verticalAlignment = Alignment.CenterVertically,
+        modifier = modifier.padding(bottom = 8.dp)
+    ) {
+        // Play btn
+        Button(
+            onClick = onPlayClick,
+            shape = MusicShapes.small,
+            modifier = Modifier
+                .padding(horizontal = 8.dp)
+                .weight(0.5f)
+        ) {
+            Icon(
+                imageVector = Icons.Filled.PlayArrow,
+                contentDescription = stringResource(R.string.icon_play)
+            )
+            Text("PLAY")
+        }
+
+        // Shuffle btn
+        Button(
+            onClick = onShuffleClick,
+            shape = MusicShapes.small,
+            modifier = Modifier
+                .padding(horizontal = 8.dp)
+                .weight(0.5f)
+        ) {
+            Icon(
+                imageVector = Icons.Filled.Shuffle,
+                contentDescription = stringResource(R.string.icon_shuffle)
+            )
+            Text("SHUFFLE")
+        }
+    }
+}
