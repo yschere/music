5e7ea4523d54751688ea702ac7d440e6cb45a491
yschere <6558581+yschere@users.noreply.github.com>
2025-09-23 21:07:30 -0500

    Consolidated ItemCountSortSelect and PlayShuffle files into one file ContentRowSets.kt

--- app/src/main/java/com/example/music/ui/shared/ContentRowSets.kt
+++ app/src/main/java/com/example/music/ui/shared/ContentRowSets.kt
@@ -4,13 +4,20 @@ import androidx.annotation.PluralsRes
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.PlayArrow
+import androidx.compose.material.icons.filled.Shuffle
+import androidx.compose.material3.Button
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
+import com.example.music.R
 import com.example.music.util.AddToPlaylistBtn
 import com.example.music.util.CreatePlaylistBtn
 import com.example.music.util.MultiSelectBtn
@@ -54,9 +61,10 @@ fun ItemCountAndSortSelectButtons(
 
 /**
  * Content section: Item Count and Plus, Sort, Select buttons
- * This is specifically for Library.Playlists and PlaylistDetails screens.
- * On Library.Playlists, onPlusClick is for create playlists flow.
- * On PlaylistDetails, onPlusClick is for add songs to playlist flow.
+ * This is specifically for Library.Playlists and PlaylistDetails screens where createOrAdd
+ * defines the intended flow for onPlusClick.
+ * On Library.Playlists, onPlusClick is for create playlists flow, so createOrAdd should be TRUE.
+ * On PlaylistDetails, onPlusClick is for add songs to playlist flow, so createOrAdd should be FALSE.
  */
 @Composable
 fun ItemCountAndPlusSortSelectButtons(
@@ -96,5 +104,48 @@ fun ItemCountAndPlusSortSelectButtons(
 
         // Multi-Select btn
         MultiSelectBtn(onClick = onSelectClick)
+    }
+}
+
+/**
+ * Content section: Play Items and Shuffle Items buttons
+ */
+@Composable
+fun PlayShuffleButtons(
+    onPlayClick: () -> Unit,
+    onShuffleClick: () -> Unit,
+    modifier: Modifier = Modifier,
+) {
+    Row(
+        verticalAlignment = Alignment.CenterVertically,
+        modifier = modifier.padding(bottom = 8.dp)
+    ) {
+        // Play btn
+        Button(
+            onClick = onPlayClick,
+            modifier = Modifier
+                .padding(horizontal = 8.dp)
+                .weight(0.5f)
+        ) {
+            Icon(
+                imageVector = Icons.Filled.PlayArrow,
+                contentDescription = stringResource(R.string.icon_play)
+            )
+            Text("PLAY")
+        }
+
+        // Shuffle btn
+        Button(
+            onClick = onShuffleClick,
+            modifier = Modifier
+                .padding(horizontal = 8.dp)
+                .weight(0.5f)
+        ) {
+            Icon(
+                imageVector = Icons.Filled.Shuffle,
+                contentDescription = stringResource(R.string.icon_shuffle)
+            )
+            Text("SHUFFLE")
+        }
     }
 }
