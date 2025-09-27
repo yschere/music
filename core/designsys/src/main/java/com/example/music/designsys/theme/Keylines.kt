34bedab4b853dc9b8abce2fd403929d35b02e9d5
yschere <6558581+yschere@users.noreply.github.com>
2025-09-26 21:31:27 -0500

    Created new set of 'const' dp values in DesignSys Keylines.kt

--- core/designsys/src/main/java/com/example/music/designsys/theme/Keylines.kt
+++ core/designsys/src/main/java/com/example/music/designsys/theme/Keylines.kt
@@ -2,4 +2,43 @@ package com.example.music.designsys.theme
 
 import androidx.compose.ui.unit.dp
 
-val Keyline1 = 16.dp
+// KeyLines
+val KEY_LINE_1 = 16.dp
+
+// Image size values
+val ITEM_IMAGE_CARD_SIZE = 160.dp
+val ITEM_IMAGE_ROW_SIZE = 56.dp
+
+// Padding values
+val SMALL_PADDING = 4.dp
+val CONTENT_PADDING = 8.dp
+val DEFAULT_PADDING = 12.dp // original horizontal padding I used
+val SCREEN_PADDING = 16.dp
+val MARGIN_PADDING = 16.dp
+val MODAL_CONTENT_PADDING = 24.dp
+
+// Icon and Button size values
+val FAB_SIZE = 56.dp
+val ICON_SIZE = 56.dp
+val PRIMARY_BUTTON_SIZE = 72.dp
+val SIDE_BUTTON_SIZE = 48.dp
+
+// Content margin size values
+val CONTENT_LEFT_MARGIN = 72.dp
+val TOOLBAR_HEIGHT = 56.dp
+val TOP_BAR_COLLAPSED_HEIGHT = 56.dp // is TopAppBar when bar is collapsed and screen scrolled down
+val TOP_BAR_EXPANDED_HEIGHT = 240.dp // is TopAppBar with art and title side by side <-> covers album and playlist details headers
+val LARGE_TOP_BAR_EXPANDED_HEIGHT = 300.dp // is for TopAppBar with large art cover
+val SUBTITLE_HEIGHT = 48.dp
+//val LIST_ITEM_HEIGHT = 72.dp //original, but this so big
+val ROW_ITEM_HEIGHT = 64.dp
+val LIST_ITEM_HEIGHT = 56.dp
+val ICON_BUTTON_RIGHT_MARGIN = 32.dp
+// large image dimension: 3:2
+
+// Navigation Drawer size values
+val NAV_DRAWER_MARGINS = 16.dp
+val NAV_DRAWER_CONTENT_LEFT_MARGIN = 72.dp // if has icon or avatar
+val NAV_DRAWER_ITEMS_HEIGHT = 48.dp
+val NAV_DRAWER_CONTENT_PADDING = 8.dp // between different content sections
+// large image dimension: 16:9
